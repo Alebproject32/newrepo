88a228289651ed37ad2ffea8c44ab0b7db2acc28
Alebproject32 <isaacale25@gmail.com>
2025-11-20 22:50:39 -0400

    I added 404 Message Error.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@
  *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
+const Util = require("./utilities/")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static") // comment
@@ -32,11 +33,16 @@ app.use(static) // comment
 // Index route
 // app.get("/", function(req, res){
   //res.render("index", {title: "Home"})}) // Last route
-  
+
 app.get("/", baseController.buildHome) // New route
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, You are lost. No, we appear to have lost that page.'})
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -45,8 +51,23 @@ const port = process.env.PORT || process.env.LOCAL_PORT || 3000;
 const host = process.env.HOST || '0.0.0.0';
 
 /* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await Util.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
+/* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
